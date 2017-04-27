a3fe247a12db1b9c9b3b98fa496b4581e5492880
hjairhc <ic16hhc@antares.dci.uia.mx>
2017-04-27 09:44:36 -0500

    Mi primer commit

---
+++ Examen.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+int main()
+{
+
+	FILE *Archivo;
+	char *token;
+	char linea[100];
+	int cont=0;
+
+	Archivo=fopen("libro.csv","rt");
+	if(!Archivo==NULL)
+	{
+		printf("No existe");
+	}
+	else
+	{
+		printf("El archivo se leyo correctamente");
+	}
+	while(!feof(Archivo))
+	{
+		fgets(linea, 50, Archivo);
+	}
+
+	printf("%s", linea);
+}
