3672b166df50e38fb052b7cbf1b612e7b69f0b95
Pichoman <ic14dcl@antares.dci.uia.mx>
2017-05-02 11:13:07 -0500

    Aplicador de examenes: Verifica si el usuario existe en la base de datos

---
+++ .c
@@ -0,0 +1,20 @@
+#include <tsdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main (void)
+{
+  int cuenta;
+  char nombre[30];
+  int examen[20];
+
+  printf("\n\tBienvenido- Universidad Iberoamericana.\n");
+  printt("\nPor favor, ingrese su numero de cuenta: \n");
+  scanf("\%d", &cuenta);
+
+
+  printf("\ningrese el nombre del examen que va  
+
+
+}
