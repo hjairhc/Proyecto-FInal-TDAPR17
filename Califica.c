4e619039d9b4f9b11c9c3a5b5047c0eaf5ceb91c
ItzelPB <ic15ipb@antares.dci.uia.mx>
2017-05-04 12:16:45 -0500

    Este es un prototipo de la funcion que califica

---
+++ Califica.c
@@ -0,0 +1,10 @@
+/*Este mes el prototipo de la funcion que calificará*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+//#define TRUE 0
+
+void Califica (int punt_totales, int punt_obtenidos, int correctas, int num_cuenta, int segundos, char *HojaResultados);
