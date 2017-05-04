90db47382d3a445e85273b35d4ece24bf74e0e59
ItzelPB <ic15ipb@antares.dci.uia.mx>
2017-05-04 12:47:54 -0500

    Falta probar y guardar en archivo

--- Califica.c
+++ Califica.c
@@ -7,4 +7,27 @@
 
 //#define TRUE 0
 
-void Califica (int punt_totales, int punt_obtenidos, int correctas, int num_cuenta, int segundos, char *HojaResultados);
+void Califica (int punt_totales, int punt_obtenidos, int correctas, int preg_totales, int num_cuenta, int segundos, char *HojaResultados);
+
+
+void Califica (int punt_totales, int punt_obtenidos, int correctas, int num_cuenta, int segundos, char *HojaResultados){
+
+  float calificacion;
+  float min;
+  int minutos=0, seg=0;
+
+
+  calificacion= (punt_obtenidos*10)/punt_totales;
+
+  if(segundos>=60){
+    min= segundos/60.0;
+    minutos=min;
+    seg= segundos%60; 
+  }
+
+  /*Se va a imprimir en la hoha de resultados: 
+  num_cuenta, calificacion, correctas/totales, minutos:seg, puntosObtenidos/puntosTotales    */
+
+  printf("%d, %.2f, %d/%d, %d:%d, %d/%d", num_cuenta, calificacion, correctas, preg_totales, minutos, seg, punt_obtenidos, punt_totales);
+
+}//funcion Califica
